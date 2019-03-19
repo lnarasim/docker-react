3291b59e62bf55c2acbd19cc372d3f1213b359c6
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2019-03-19 11:24:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bye there, loads automagically
+            Another deployment from lnarasim.
           </p>
           <a
             className="App-link"
